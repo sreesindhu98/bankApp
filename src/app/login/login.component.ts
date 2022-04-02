fd8c4194f4567fbd4c52d045343ae6acf51447c4
sree <sreesindhu98@gmail.com>
2022-04-01 13:28:17 -1200

    data added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  aim="Your data is displayed"
+  accno="Account number please!"
+  acno=""
+  pswd=""
+// database
+dtabase={
+  1000:{accno:1000,uname:"sree",password:123,balance:3000},
+  1001:{accno:1000,uname:"sree",password:123,balance:4000},
+  1002:{accno:1000,uname:"sree",password:123,balance:5000}
+}
   constructor() { }
 
   ngOnInit(): void {
+   
   }
-
+  acnoChange(event:any){
+    this.acno=event.target.value
+    console.log(this.acno);
+    
+  }
+  pswdChange(event:any){
+    this.pswd=event.target.value
+    console.log(this.pswd);
+    
+  }
+  login(){
+    alert("Login clicked")
+      
+  }
 }
